8f2d4a537fa228a3eafa2ad88dd691cb34b6d4d1
Vladimir Lyulin <Patsi2@yandex.ru>
2025-10-10 14:36:12 +0400

    Use completely new filenames for Console screenshots to bypass all caching

--- frontend/app/devtools/page.tsx
+++ frontend/app/devtools/page.tsx
@@ -91,8 +91,8 @@ export default function DevToolsPage() {
           category: "Console",
           completed: false,
           challenge: "Используйте консоль DevTools для выполнения JavaScript команд: 1) Выведите сообщение 'Привет, DevTools!' в консоль, 2) Найдите демо-элемент внизу страницы с помощью консольной команды (синий блок с градиентом), 3) Измените его заголовок с 'Демо элемент для практики' на 'Новый заголовок элемента!'. Консоль - это мощный инструмент для отладки и тестирования JavaScript кода.",
-          beforeImage: "/screenshots/devtools/console-before-v2.svg",
-          afterImage: "/screenshots/devtools/console-after-v2.svg",
+          beforeImage: "/screenshots/devtools/console-before-new.svg",
+          afterImage: "/screenshots/devtools/console-after-new.svg",
           solution: {
             steps: [
               "1. Откройте DevTools (F12 или Ctrl+Shift+I)",
